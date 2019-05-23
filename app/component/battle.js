a8b2ceb0fe71ce63a087d0c03f6cfe5f1097b5fd
Tamilarasu <tamilarasu.tp@gmail.com>
2019-05-23 20:21:15 +0530

    React routers

---
+++ app/component/battle.js
@@ -0,0 +1,13 @@
+var React = require('react')
+
+class Battle extends React.Component {
+    render() {
+        return (
+            <div>
+                Battle will begun shortly!
+            </div>
+        )
+    }
+}
+
+module.exports = Battle
